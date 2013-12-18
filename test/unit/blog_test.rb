a4fb227a10e3714a027ee0f2b90d325f1711b18a
Jon Hinson <hinson.jonathan@gmail.com>
2013-12-18 10:41:48 -0600

    Use more descriptive assertion

--- test/unit/blog_test.rb
+++ test/unit/blog_test.rb
@@ -4,6 +4,6 @@ class BlogTest < ActiveSupport::TestCase
   test "records of has_many association get duplicated in memory" do
     blog = Blog.create(name: 'Foo Blog')
     blog.posts << Post.new(title: 'Blah', body: 'Some post')
-    assert blog.posts.to_a.count == 1
+    assert_equal 1, blog.posts.to_a.count
   end
 end
