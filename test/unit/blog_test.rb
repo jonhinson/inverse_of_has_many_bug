ee6acff7cfd32b6176bf127358252d67d65f712a
Jon Hinson <hinson.jonathan@gmail.com>
2013-12-18 10:29:34 -0600

    Update README to provide short explanation

--- test/unit/blog_test.rb
+++ test/unit/blog_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class BlogTest < ActiveSupport::TestCase
-  test "the truth" do
+  test "records of has_many association get duplicated in memory" do
     blog = Blog.create(name: 'Foo Blog')
     blog.posts << Post.new(title: 'Blah', body: 'Some post')
     assert blog.posts.to_a.count == 1
